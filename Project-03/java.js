dd7155eb8de1b210b751fa53e9b58f720a2f1396
hunterxmckenzie <98671049+hunterxmckenzie@users.noreply.github.com>
2022-05-03 18:59:59 -0400

    -

--- Project-03/java.js
+++ Project-03/java.js
@@ -30,287 +30,280 @@
       name: "1994 'Blue Album' Tour",
       color: "1990's",
       image:
-        "https://www.dropbox.com/s/dm14q2oh4l69x82/1994%20Weezer%20Blue%20Album%20Tour.jpg?dl=0"
+        "https://www.dropbox.com/s/dm14q2oh4l69x82/1994%20Weezer%20Blue%20Album%20Tour.jpg?raw=1"
      , band: "Weezer"
       },
     {
       name: "2019 'Father of the Bride' Bootleg",
       color: "2010's",
       image:
-        "https://www.dropbox.com/s/7lns89fp0x5p8q0/2019%20Father%20of%20the%20Bride%20Bootleg.jpg?dl=0"
+        "https://www.dropbox.com/s/7lns89fp0x5p8q0/2019%20Father%20of%20the%20Bride%20Bootleg.jpg?raw=1"
       ,band: "Vampire Weekend"
       },
     {
       name: "2013 'Modern Vampires of the City' Tour",
       color: "2010's",
       image:
-     "https://www.dropbox.com/s/c5i6fdiqv4ne3hy/2013%20Modern%20Vampires%20of%20the%20City%20Tour.jpg?dl=0"
+     "https://www.dropbox.com/s/c5i6fdiqv4ne3hy/2013%20Modern%20Vampires%20of%20the%20City%20Tour.jpg?raw=1"
      ,band: "Vampire Weekend"
     },
     {
       name: "2013 'Modern Vampires of the City' Tour Alternate",
       color: "2010's",
       image:
-     "https://www.dropbox.com/s/x9oi7a28fra4wum/2013%20Modern%20Vampires%20of%20the%20City%20Tour%202.jpg?dl=0"
+     "https://www.dropbox.com/s/x9oi7a28fra4wum/2013%20Modern%20Vampires%20of%20the%20City%20Tour%202.jpg?raw=1"
      ,band: "Vampire Weekend"
     },
     {
       name: "2020 'The New Abnormal' Album Release Bootleg",
       color: "2020's",
       image:
-     "https://www.dropbox.com/s/fbdo458328uww9w/2020%20The%20Strokes%20Bootleg%20New%20Abnormal.jpg?dl=0"
+     "https://www.dropbox.com/s/fbdo458328uww9w/2020%20The%20Strokes%20Bootleg%20New%20Abnormal.jpg?raw=1"
      ,band: "The Strokes"
     },
     {
       name: "2002 'Is This It' Tour",
       color: "2000's",
       image:
-     "https://www.dropbox.com/s/mnw955bhtsia2xc/2002%20The%20Strokes%20Is%20This%20It%20Tour.jpg?dl=0"
+     "https://www.dropbox.com/s/mnw955bhtsia2xc/2002%20The%20Strokes%20Is%20This%20It%20Tour.jpg?raw=1"
     , band: "The Strokes"
     },
     {
       name: "2001 'Is This It' Tour",
       color: "2000's",
       image:
-     "https://www.dropbox.com/s/6fm1rwap36zwrdp/2001%20The%20Strokes%20Is%20This%20It%20Tour.jpg?dl=0"
+     "https://www.dropbox.com/s/6fm1rwap36zwrdp/2001%20The%20Strokes%20Is%20This%20It%20Tour.jpg?raw=1"
     , band: "The Strokes"
     },
     {
       name: "1982 'Come Dancing' Tour",
       color: "1980's",
       image:
-     "https://www.dropbox.com/s/7xk8uruuzeb6xdh/02122149-1982-The-Kinks-Come-Dancing-Tour-Shirt-FRONT.jpg?dl=0"
-     ,band: "The Kinks"
-    },
-    {
-      name: "1980 'Low Budget' Tour",
-      color: "1980's",
-      image:
-     "https://www.dropbox.com/s/7xk8uruuzeb6xdh/02122149-1982-The-Kinks-Come-Dancing-Tour-Shirt-FRONT.jpg?dl=0"
+     "https://www.dropbox.com/s/7xk8uruuzeb6xdh/02122149-1982-The-Kinks-Come-Dancing-Tour-Shirt-FRONT.jpg?raw=1"
      ,band: "The Kinks"
     },
     {
       name: "1980 'One For the Road' Tour",
       color: "1980's",
       image:
-     "https://www.dropbox.com/s/k827ik0zakasqk6/3a4b409ffd68c89f6038b639a72782cd.jpg?dl=0"
+     "https://www.dropbox.com/s/k827ik0zakasqk6/3a4b409ffd68c89f6038b639a72782cd.jpg?raw=1"
      ,band: "The Kinks"
     },
     {
       name: "2022 Bootleg",
       color: "2020's",
       image:
-     "https://www.dropbox.com/s/zdv5lvj8hhxowws/2022%20Bootleg.jpg?dl=0"
+     "https://www.dropbox.com/s/zdv5lvj8hhxowws/2022%20Bootleg.jpg?raw=1"
       ,band: "Rolling Stones"
     },
     {
       name: "1997 'Bridges To Babylon' Tour",
       color: "1990's",
       image:
-     "https://www.dropbox.com/s/o840x92gylgv9if/1997%20Bridges%20To%20Babylon.jpg?dl=0"
+     "https://www.dropbox.com/s/o840x92gylgv9if/1997%20Bridges%20To%20Babylon.jpg?raw=1"
       ,band: "Rolling Stones"
     },
     {
       name: "1981 USA Tour",
       color: "1980's",
       image:
-     "https://www.dropbox.com/s/yw78qul4nczyhp4/1981%20Tour.jpg?dl=0"
+     "https://www.dropbox.com/s/yw78qul4nczyhp4/1981%20Tour.jpg?raw=1"
       ,band: "Rolling Stones"
     },
     {
       name: "1981 USA Tour Alternate",
       color: "1980's",
       image:
-     "https://www.dropbox.com/s/3y5gre1t0fj5r66/1981%20Tour%202.jpg?dl=0"
+     "https://www.dropbox.com/s/3y5gre1t0fj5r66/1981%20Tour%202.jpg?raw=1"
       ,band: "Rolling Stones"
     },
     {
       name: "1978 USA Tour Bootleg",
       color: "1970's",
       image:
-     "https://www.dropbox.com/s/2gi3cddndjny55u/1978%20Tour.jpg?dl=0"
+     "https://www.dropbox.com/s/2gi3cddndjny55u/1978%20Tour.jpg?raw=1"
       ,band: "Rolling Stones"
     },
     {
       name: "1975 USA Tour",
       color: "1970's",
       image:
-     "https://www.dropbox.com/s/xit2lr5nr9680k4/1975%20Tour.jpg?dl=0"
+     "https://www.dropbox.com/s/xit2lr5nr9680k4/1975%20Tour.jpg?raw=1"
       ,band: "Rolling Stones"
     },
     {
       name: "1972 USA Tour",
       color: "1970's",
       image:
-     "https://www.dropbox.com/s/g2bgjz0vyv2moxu/1972%20Concert.jpg?dl=0"
+     "https://www.dropbox.com/s/g2bgjz0vyv2moxu/1972%20Concert.jpg?raw=1"
       ,band: "Rolling Stones"
     },
     {
       name: "2006 'Stadium Arcadium' USA Tour",
       color: "2000's",
       image:
-     "https://www.dropbox.com/s/5d14z43bzixtea0/s-l300.jpg?dl=0"
+     "https://www.dropbox.com/s/5d14z43bzixtea0/s-l300.jpg?raw=1"
       ,band: "Red Hot Chili Peppers"
     },
     {
       name: "2006 'Stadium Arcadium' USA Tour Alternate",
       color: "2000's",
       image:
-     "https://www.dropbox.com/s/nhae0hed34pjw7y/m_5d08058ea2e32e4048488ac2.jpeg?dl=0"
+     "https://www.dropbox.com/s/nhae0hed34pjw7y/m_5d08058ea2e32e4048488ac2.jpeg?raw=1"
       ,band: "Red Hot Chili Peppers"
     },
     {
       name: "1996 'Blood, Sugar, Sex, Magik' USA Tour",
       color: "1990's",
       image:
-     "https://www.dropbox.com/s/xizvdqe5elbltzc/IMG_6754.jpg?dl=0"
+     "https://www.dropbox.com/s/xizvdqe5elbltzc/IMG_6754.jpg?raw=1"
       ,band: "Red Hot Chili Peppers"
     },
     {
       name: "2016 'A Moon Shaped Pool' World Tour",
       color: "2010's",
       image:
-     "https://www.dropbox.com/s/u1xzqhoou6rh5db/2016%20A%20Moon%20Shaped%20Pool.jpg?dl=0"
+     "https://www.dropbox.com/s/u1xzqhoou6rh5db/2016%20A%20Moon%20Shaped%20Pool.jpg?raw=1"
       ,band: "Radiohead"
     },
     {
       name: "2016 'A Moon Shaped Pool' World Tour Alternate",
       color: "2010's",
       image:
-     "https://www.dropbox.com/s/9wva448hr52c998/2016%20A%20Moon%20Shaped%20Pool%202.jpg?dl=0"
+     "https://www.dropbox.com/s/9wva448hr52c998/2016%20A%20Moon%20Shaped%20Pool%202.jpg?raw=1"
       ,band: "Radiohead"
     },
     {
       name: "2006 'Hail To The Thief' Tour ",
       color: "2000's",
       image:
-     "https://www.dropbox.com/s/wix2y744veuq7xv/2006%20Hail%20To%20The%20Thief.jpg?dl=0"
+     "https://www.dropbox.com/s/wix2y744veuq7xv/2006%20Hail%20To%20The%20Thief.jpg?raw=1"
       ,band: "Radiohead"
     },
     {
       name: "1998 'OK Computer' Tour ",
       color: "1990's",
       image:
-     "https://www.dropbox.com/s/u1a73btswhusbqn/1998%20OK%20Computer%20tour.jpg?dl=0"
+     "https://www.dropbox.com/s/u1a73btswhusbqn/1998%20OK%20Computer%20tour.jpg?raw=1"
       ,band: "Radiohead"
     },
     {
       name: "1993 'Vs.' Tour ",
       color: "1990's",
       image:
-     "https://www.dropbox.com/s/0smwx0ubs3mv0m4/1993%20Pearl%20Jam%20Vs.%20Tour.jpg?dl=0"
+     "https://www.dropbox.com/s/0smwx0ubs3mv0m4/1993%20Pearl%20Jam%20Vs.%20Tour.jpg?raw=1"
       ,band: "Pearl Jam"
     },
     {
       name: "1994 'Alive' Tour ",
       color: "1990's",
       image:
-     "https://www.dropbox.com/s/0dbxs2pdif1p47f/1993%20Pearl%20Jam%20Alive%20Shirt.jpg?dl=0"
+     "https://www.dropbox.com/s/0dbxs2pdif1p47f/1993%20Pearl%20Jam%20Alive%20Shirt.jpg?raw=1"
       ,band: "Pearl Jam"
     },
     {
       name: "1992 'Don't Give Up' European Tour ",
       color: "1990's",
       image:
-     "https://www.dropbox.com/s/343hk4whx797xaw/1992%20Pearl%20Jam%20Dont%20Give%20Up%20Europe%20Tour.jpg?dl=0"
+     "https://www.dropbox.com/s/343hk4whx797xaw/1992%20Pearl%20Jam%20Dont%20Give%20Up%20Europe%20Tour.jpg?raw=1"
       ,band: "Pearl Jam"
     },
     {
       name: "1984 'Ride The Lightning' Tour ",
       color: "1980's",
       image:
-     "https://www.dropbox.com/s/8ro4k645gbon4fj/Ride%20the%20Lightning%20Tour.jpg?dl=0"
+     "https://www.dropbox.com/s/8ro4k645gbon4fj/Ride%20the%20Lightning%20Tour.jpg?raw=1"
       ,band: "Metallica"
     },
     {
       name: "1996 'Load' Tour ",
       color: "1990's",
       image:
-     "https://www.dropbox.com/s/a4yqfef20m61tdp/1996%20Load%20Album%20Tour.jpg?dl=0"
+     "https://www.dropbox.com/s/a4yqfef20m61tdp/1996%20Load%20Album%20Tour.jpg?raw=1"
       ,band: "Metallica"
     },
     {
       name: "1992 'Wherever I May Roam' Tour",
       color: "1990's",
       image:
-     "https://www.dropbox.com/s/aqwvg5zez64jw2y/1992%20Wherever%20I%20May%20Roam.jpg?dl=0"
+     "https://www.dropbox.com/s/aqwvg5zez64jw2y/1992%20Wherever%20I%20May%20Roam.jpg?raw=1"
       ,band: "Metallica"
     },
     {
       name: "1991 World Tour",
       color: "1990's",
       image:
-     "https://www.dropbox.com/s/wy1joq3xc7copn5/1991%20World%20Tour.jpg?dl=0"
+     "https://www.dropbox.com/s/wy1joq3xc7copn5/1991%20World%20Tour.jpg?raw=1"
       ,band: "Metallica"
     },
     {
       name: "1989 'Their Money Tips Her Scales Again' Tour ",
       color: "1980's",
       image:
-     "https://www.dropbox.com/s/g52mhfrsu5wh93q/1989%20Their%20Money%20Tips%20Her%20Scales%20Again.jpg?dl=0"
+     "https://www.dropbox.com/s/g52mhfrsu5wh93q/1989%20Their%20Money%20Tips%20Her%20Scales%20Again.jpg?raw=1"
       ,band: "Metallica"
     },
     {
       name: "2000 'The Farewell' Tour",
       color: "2000's",
       image:
-     "https://www.dropbox.com/s/q3akq6u8j9v5qk6/2000%20the%20farewell%20tour.jpg?dl=0"
+     "https://www.dropbox.com/s/q3akq6u8j9v5qk6/2000%20the%20farewell%20tour.jpg?raw=1"
       ,band: "KISS"
     },
     {
       name: "1996 'Alive Worldwide' Tour",
       color: "1990's",
       image:
-     "https://www.dropbox.com/s/jejrayymxzj1jxp/1996%20Alive%20Worldwide.jpg?dl=0"
+     "https://www.dropbox.com/s/jejrayymxzj1jxp/1996%20Alive%20Worldwide.jpg?raw=1"
       ,band: "KISS"
     },
     {
       name: "1990 'Hot Shade' Tour",
       color: "1990's",
       image:
-     "https://www.dropbox.com/s/6ruhosbadg1644n/1990%20Hot%20Shade%20Tour.jpg?dl=0"
+     "https://www.dropbox.com/s/6ruhosbadg1644n/1990%20Hot%20Shade%20Tour.jpg?raw=1"
       ,band: "KISS"
     },
     {
       name: "1987 Tour",
       color: "1980's",
       image:
-     "https://www.dropbox.com/s/lpjcwky5n71b67d/1987%20Tour.jpg?dl=0"
+     "https://www.dropbox.com/s/lpjcwky5n71b67d/1987%20Tour.jpg?raw=1"
       ,band: "KISS"
     },
     {
       name: "1978 Tour",
       color: "1970's",
       image:
-     "https://www.dropbox.com/s/mdfrn25j60mr2qx/1978%20Tour.JPG?dl=0"
+     "https://www.dropbox.com/s/mdfrn25j60mr2qx/1978%20Tour.JPG?raw=1"
       ,band: "KISS"
     },
     {
       name: "1997 Tour",
       color: "1990's",
       image:
-     "https://www.dropbox.com/s/94jlpatbml4vm9w/1997%20Tour.jpg?dl=0"
+     "https://www.dropbox.com/s/94jlpatbml4vm9w/1997%20Tour.jpg?raw=1"
       ,band: "Green Day"
     },
     {
       name: "1995 'Dookie' Tour",
       color: "1990's",
       image:
-     "https://www.dropbox.com/s/nkez2ip8de5w2ae/1995%20Dookie%20Tour%202.jpg?dl=0"
+     "https://www.dropbox.com/s/nkez2ip8de5w2ae/1995%20Dookie%20Tour%202.jpg?raw=1"
       ,band: "Green Day"
     },
     {
       name: "1995 'Dookie' Tour Alternate",
       color: "1990's",
       image:
-     "https://www.dropbox.com/s/6muydw8ise5lo1r/1995%20Dookie%20Tour%201.png?dl=0"
+     "https://www.dropbox.com/s/6muydw8ise5lo1r/1995%20Dookie%20Tour%201.png?raw=1"
       ,band: "Green Day"
     },
     {
       name: "1994 'Self-Titled' Alternate",
       color: "1990's",
       image:
-     "https://www.dropbox.com/s/ykxp04t06zcvfdu/1994%20Green%20Day%20Tour.jpg?dl=0",
+     "https://www.dropbox.com/s/ykxp04t06zcvfdu/1994%20Green%20Day%20Tour.jpg?raw=1",
       band: "Green Day"
     },
   ];
@@ -336,7 +329,7 @@
       let image = document.createElement("img");
       image.setAttribute("src", Shirts[i].image);
       // band name
-      let artist = document.createElement("p");
+      let artist = document.createElement("h4");
       artist.textContent = Shirts[i].band;
       // append created elements to page
       ul.appendChild(list_item);
@@ -348,7 +341,7 @@
   }
   renderShirtsToPage(Shirts);
   
-  //---------- FILTER shirts BY COLOR
+  //---------- FILTER shirts BY decade
   
   let filterBtns = document.querySelector(".filters");
   let cards = document.querySelectorAll(".card");
@@ -376,11 +369,54 @@
   }
   filterBtns.addEventListener("click", filterFn);
   
+// add ascending descending here (sorting function)
+
+function sortingFn(e) {
+  if (e.target.classList.contains("filter-btn")) {
+    const filterValue = e.target.getAttribute("data-filter");
+ 
+ if (filterValue === "ascending") {
+  // [1] sort the object
+  Shirts.sort(function (a, b) {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  });
+  console.log(Shirts);
+  ul.innerHTML = "";
+  renderShirtsToPage(Shirts);
+} else if (filterValue === "descending") {
+  // [2] reverse sort the object
+  Shirts.reverse();
+  console.log(Shirts);
+  ul.innerHTML = "";
+  renderShirtsToPage(Shirts);
+}
+
+//attempts
+function sortingFn(selector) {
+  $(selector).find("li").sort(function(a, b) {
+      return(Number(a.innerHTML) - Number(b.innerHTML));
+  }).each(function(index, el) {
+      $(el).parent().append(el);
+  });
+}
+
+
+
+filterBtns.addEventListener("click", sortingFn);}}
+
   //search engine for name of shirts, currently without images//
 
   function searchShirts(event){
     let searchString = event.target.value;
      console.log(searchString)
+
+
  
      const searchedName = Shirts.filter(function(Shirts){
          if(searchString){
@@ -389,7 +425,8 @@
      })
      renderSearchedShirts(searchedName)
  }
- 
+
+
  function renderSearchedShirts(searched){
  console.log('line 64:', searched)
  clearList();
@@ -405,38 +442,7 @@
  function clearList(){
      ul.innerHTML = ""
  }
- 
+
  searchBar.addEventListener('keyup', searchShirts)
+ 
  //From class, ascending and descending//
-
- function sortingFn(event){
-     console.log(event.target);
-     if(event.target.classList.contains("filter-btn")){
-         //do something
-        const filterValue = event.target.getAttribute("data-filter");
-
-        if(filterValue == "ascending"){
-            //write sort logic
-            flowers.sort(function(a, b){
-                if(a.name < b.name){
-                    return -1;
-                }
-                if(a.name > b.name){
-                    return 0;
-                }
-            })
-            console.log('sorted', flowers);
-            ul.innerHTML =""
-            renderShirtsToPage(Shirts)
-
-        } else if (filterValue === "descending") {
-            //write reverse logic
-            Shirts.reverse();
-            console.log('reversed', Shirts)
-            //clear the page of unsorted shirts
-            ul.innerHTML ="";
-            renderShirtsToPage(Shirts)
-
-        }
-     }
-    }
